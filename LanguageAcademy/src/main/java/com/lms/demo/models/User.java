cfe43610137192bfda1c95557de5a0c908d9e765
AndersonGG21 <andersongarcesgarcia@gmail.com>
2022-10-24 17:58:02 -0500

    Add father class for all Users

---
+++ LanguageAcademy/src/main/java/com/lms/demo/models/User.java
@@ -0,0 +1,13 @@
+package com.lms.demo.models;
+
+/**
+ *
+ * @author Anderson
+ */
+public class User {
+    private Long id;
+    private String email;
+    private String phoneNumber;
+    private String roleName;
+    private String password;
+}
