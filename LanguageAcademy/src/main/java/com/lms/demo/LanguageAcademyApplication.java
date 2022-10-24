a7134198f65844129b6008afed6b91b13cec6ef7
AndersonGG21 <andersongarcesgarcia@gmail.com>
2022-10-24 16:16:52 -0500

    (build): Generate project structure

---
+++ LanguageAcademy/src/main/java/com/lms/demo/LanguageAcademyApplication.java
@@ -0,0 +1,13 @@
+package com.lms.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LanguageAcademyApplication.class, args);
+	}
+
+}
