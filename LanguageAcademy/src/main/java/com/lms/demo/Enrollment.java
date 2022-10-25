755b37f2b6a4d0a022da146370636e7d43a1901b
AndersonGG21 <andersongarcesgarcia@gmail.com>
2022-10-25 11:17:16 -0500

    refactor: Remove redundant imports on Enrollment class

--- LanguageAcademy/src/main/java/com/lms/demo/Enrollment.java
+++ LanguageAcademy/src/main/java/com/lms/demo/Enrollment.java
@@ -1,20 +1,12 @@
 package com.lms.demo;
 
-import javax.annotation.Generated;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+
+import javax.persistence.*;
 
 import com.lms.demo.models.Student;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+
 
 @Getter
 @Setter
