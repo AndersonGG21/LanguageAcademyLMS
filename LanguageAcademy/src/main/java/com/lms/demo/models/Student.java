e5f68b95d51efed3452d95244a8d12920a8f9f89
AndersonGG21 <andersongarcesgarcia@gmail.com>
2022-10-25 10:58:53 -0500

    Add Student class model

---
+++ LanguageAcademy/src/main/java/com/lms/demo/models/Student.java
@@ -0,0 +1,18 @@
+package com.lms.demo.models;
+
+import javax.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "students")
+public class Student extends User {
+    
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "age")
+    private int age;
+}
