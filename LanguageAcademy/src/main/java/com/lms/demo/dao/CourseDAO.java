50a0ff7ee7e4bc94ac15fbccc06f6fc85ebdb611
AndersonGG21 <andersongarcesgarcia@gmail.com>
2022-10-26 13:07:28 -0500

    Add CourseDAO interface

---
+++ LanguageAcademy/src/main/java/com/lms/demo/dao/CourseDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lms.demo.dao;
+
+import com.lms.demo.models.Course;
+import java.util.List;
+
+/**
+ *
+ * @author Anderson
+ */
+public interface CourseDAO {
+    
+    List<Course> getCourses();
+    void deleteCourse(String courseCode);
+    void createCourse(Course course);
+    Course getCourse(String courseCode);
+}
