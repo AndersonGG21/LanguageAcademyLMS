983d963e4620b87f66870f205925d1fc13899049
AndersonGG21 <andersongarcesgarcia@gmail.com>
2022-10-26 13:10:49 -0500

    Add implementation for CourseDAO

---
+++ LanguageAcademy/src/main/java/com/lms/demo/dao/CourseDAOImp.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lms.demo.dao;
+
+import com.lms.demo.models.Course;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author Anderson
+ */
+@Repository
+@Transactional
+public class CourseDAOImp implements CourseDAO {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+    
+    @Override
+    public List<Course> getCourses() {
+        
+        return null;
+    }
+
+    @Override
+    public void deleteCourse(String courseCode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createCourse(Course course) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Course getCourse(String courseCode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
