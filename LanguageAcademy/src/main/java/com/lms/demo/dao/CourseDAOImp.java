0c0a9ffb6a0f2aa984aea4898a5dac9a690345dd
AndersonGG21 <andersongarcesgarcia@gmail.com>
2022-10-26 13:15:21 -0500

    feat(backend): Add getCourse method

--- LanguageAcademy/src/main/java/com/lms/demo/dao/CourseDAOImp.java
+++ LanguageAcademy/src/main/java/com/lms/demo/dao/CourseDAOImp.java
@@ -44,7 +44,7 @@ public class CourseDAOImp implements CourseDAO {
 
     @Override
     public Course getCourse(String courseCode) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return entityManager.find(Course.class, courseCode);
     }
     
 }
