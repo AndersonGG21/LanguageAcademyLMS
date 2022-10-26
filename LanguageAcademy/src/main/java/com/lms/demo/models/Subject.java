e3ea96a44f7d9cd56fd0e77e5b942d5dec5a54a5
AndersonGG21 <andersongarcesgarcia@gmail.com>
2022-10-25 23:39:17 -0500

    Add Subject class model

---
+++ LanguageAcademy/src/main/java/com/lms/demo/models/Subject.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lms.demo.models;
+
+import javax.persistence.*;
+import lombok.*;
+
+/**
+ *
+ * @author Anderson
+ */
+@Getter
+@Setter
+@ToString
+@Entity(name = "subjects")
+public class Subject {
+    @Id @Column(name = "subject_name")
+    private String name;
+    
+    @ManyToOne
+    @JoinColumn(name = "course")
+    private Course course;
+}
