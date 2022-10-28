9bc3537de7eaed8d48d174936d42f0110623a256
AndersonGG21 <andersongarcesgarcia@gmail.com>
2022-10-28 12:43:28 -0500

    Add api rest to get all groups

--- LanguageAcademy/src/main/java/com/lms/demo/controllers/GroupController.java
+++ LanguageAcademy/src/main/java/com/lms/demo/controllers/GroupController.java
@@ -5,10 +5,25 @@
  */
 package com.lms.demo.controllers;
 
+import com.lms.demo.dao.GroupDAO;
+import com.lms.demo.models.Group;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  * @author Anderson
  */
+@RestController
 public class GroupController {
     
+    @Autowired
+    private GroupDAO groupDAO;
+    
+    @GetMapping(value = "/api/groups")
+    public List<Group> getGroups(){
+        return groupDAO.getGroups();
+    }
 }
