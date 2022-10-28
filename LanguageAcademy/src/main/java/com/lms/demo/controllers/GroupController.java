d71bf10f6c20693b2268f3ea85d287d7b95e312b
AndersonGG21 <andersongarcesgarcia@gmail.com>
2022-10-28 12:49:53 -0500

    Add api rest to get groups by course

--- LanguageAcademy/src/main/java/com/lms/demo/controllers/GroupController.java
+++ LanguageAcademy/src/main/java/com/lms/demo/controllers/GroupController.java
@@ -10,6 +10,7 @@ import com.lms.demo.models.Group;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -25,5 +26,10 @@ public class GroupController {
     @GetMapping(value = "/api/groups")
     public List<Group> getGroups(){
         return groupDAO.getGroups();
+    }
+    
+    @GetMapping(value = "api/groups/{course}")
+    public List<Group> getGroupsByCourse(@PathVariable String course){
+        return groupDAO.getGroupsByCourse(course);
     }
 }
