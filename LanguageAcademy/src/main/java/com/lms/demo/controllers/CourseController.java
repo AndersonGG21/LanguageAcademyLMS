743459716ce8c3113bc766bab649e24cbf2428ea
AndersonGG21 <andersongarcesgarcia@gmail.com>
2022-10-29 11:18:06 -0500

    Add a new javascript file for admin functions

--- LanguageAcademy/src/main/java/com/lms/demo/controllers/CourseController.java
+++ LanguageAcademy/src/main/java/com/lms/demo/controllers/CourseController.java
@@ -7,12 +7,10 @@ package com.lms.demo.controllers;
 
 import com.lms.demo.dao.CourseDAO;
 import com.lms.demo.models.Course;
-import java.math.BigInteger;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
