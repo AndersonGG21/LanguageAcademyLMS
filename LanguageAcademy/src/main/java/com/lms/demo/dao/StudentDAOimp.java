7b259b25357bc670f7a69a2fd6a208ef6c69c303
Julian362 <Juliangarcia362@hotmail.com>
2022-10-28 22:55:51 -0500

    Implements methods of interface StudentsDAO

--- LanguageAcademy/src/main/java/com/lms/demo/dao/StudentDAOimp.java
+++ LanguageAcademy/src/main/java/com/lms/demo/dao/StudentDAOimp.java
@@ -8,6 +8,59 @@ package com.lms.demo.dao;
  *
  * @author seang
  */
-public class StudentDAOimp {
+import com.lms.demo.models.Student;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+@Transactional
+
+public class StudentDAOimp implements StudentDAO{
+    
+    
+    @PersistenceContext
+    EntityManager entityManager;
+    
+    @Transactional 
+    public List<Student> getStudent(){
+        String query = "FROM students";
+        return entityManager.createQuery(query).getResultList();
+    }
+
+   public Student getStudentOne(int id){
+       return entityManager.find(Student.class, id);
+   }
+   
+   @Override
+   public void regStudent(Student student){
+       Student studentNew = new Student();
+       studentNew.setId(student.getId());
+       studentNew.setEmail(student.getEmail());
+       studentNew.setName(student.getName());
+       studentNew.setPhoneNumber(student.getPhoneNumber());
+       studentNew.setRoleName(student.getRoleName());
+       studentNew.setAge(student.getAge());
+       studentNew.setAddress(student.getAddress());
+       entityManager.merge(student);
+   }
+   
+   @Override
+   public void modStudent(Student student, int id){
+       Student temp= entityManager.find(Student.class, id);
+       temp.setName(student.getName());
+       temp.setEmail(student.getEmail());
+       entityManager.merge(temp);
+   }
+   
+   @Override
+   public void delete(int id){
+       Student student = entityManager.find(Student.class, id);
+       entityManager.remove(student);
+   }
     
 }
