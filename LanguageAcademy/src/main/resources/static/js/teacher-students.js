5d8bcb8c821e96159203e7dc258a417b4f9eab7f
SergioGuerra <sergio_guerra82201@elpoli.edu.co>
2022-10-29 10:41:07 -0500

    Update download PDf whit the students list

--- LanguageAcademy/src/main/resources/static/js/teacher-students.js
+++ LanguageAcademy/src/main/resources/static/js/teacher-students.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
    $('#btn-download').click(function () {
-    $('#content').printThis();
+      $('#content').printThis({
+         importCSS: true,
+         importStyle: true,
+         printContainer: true,  
+         header: "<h1>Students List</h1>"
+       });       
    });
 });
+
