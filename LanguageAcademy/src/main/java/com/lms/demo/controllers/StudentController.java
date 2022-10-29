eaed94c4d8fd45fd99e096565992ae8f49a12936
Julian362 <Juliangarcia362@hotmail.com>
2022-10-28 22:54:13 -0500

    Create StudentController.java
    Create controller for student and add methods

---
+++ LanguageAcademy/src/main/java/com/lms/demo/controllers/StudentController.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.lms.demo.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.lms.demo.dao.StudentDAO;
+import com.lms.demo.models.Student;
+/**
+ *
+ * @author Julian
+ */
+
+@RestController
+public class StudentController {
+
+    @Autowired
+    private StudentDAO studentDAO;
+
+
+    @RequestMapping(value = "api/student/{id}", method = RequestMethod.GET)
+    public Student getStudent(@PathVariable int id){
+        return studentDAO.getStudentOne(id);
+    }
+   
+    @RequestMapping(value = "api/students", method = RequestMethod.GET)
+    public List<Student> getStudent(){
+        return studentDAO.getStudent();
+    } 
+    
+    
+    @RequestMapping (value = "api/student", method = RequestMethod.POST)
+    public void registerStudent(@RequestBody Student student){
+        studentDAO.regStudent(student);
+    }
+    
+    @RequestMapping(value = "api/student/{id}" , method = RequestMethod.PATCH)
+    public void modStudent(@RequestBody Student student, @PathVariable int id){
+        studentDAO.modStudent(student, id);
+    }
+    
+    @RequestMapping(value = "api/student/{id}", method = RequestMethod.DELETE)
+    public void deleteStudent(@PathVariable int id){
+        studentDAO.delete(id);
+    }
+}
