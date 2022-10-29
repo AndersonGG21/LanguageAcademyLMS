e9853915f13b73d62a171960955f407c8e097180
Luis david <david12.29@hotmail.com>
2022-10-29 15:35:17 -0500

    Update admin-course, new function about subjects

--- LanguageAcademy/src/main/resources/static/js/admin-courses.js
+++ LanguageAcademy/src/main/resources/static/js/admin-courses.js
@@ -4,8 +4,9 @@ $(document).ready(function () {
 
 async function createCourse(){
     let condicion=true;
+    let id=Date.now().toString();
     let data={};
-    data.code=Date.now().toString();
+    data.code=id;
     data.name = document.getElementById("recipient-name").value;
     data.img = document.getElementById("recipient-Image").value;
     data.desc = document.getElementById("message-Description").value;
@@ -29,19 +30,19 @@ async function createCourse(){
         document.getElementById("conditionDescription").classList.remove('conditionColor');
     }
     if(condicion){
-        const request = await fetch('/api/groups/' + id, {
-            method: 'PATCH',
+        const request = await fetch('/api/courses', {
+            method: 'POST',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({
-                teacher
-            })
+            body: JSON.stringify(data)
         });
+        createSubjects(id,data.name);
         cleanModalCourse();
         let btnClose=document.getElementById("btnClose");
         btnClose.click();
+    
 
     }
 
@@ -61,31 +62,12 @@ async function createCourseMaterial(){
     let list = '';
 
     for (const iterator of coursesHTML) {
-        let card = "<div class='card' style='width: 18rem'>\n\
-                        <div class='card-left'>\n\
-                            <div class='row g-0'>\n\
-                            <div class='col-md-4 img-container'>\n\
-                            <img src='/imgs/ena1.png' class='img-fluid rounded-start' alt='...'/>\n\
-                        </div>\n\
-                        <div class='col-md-8'>\n\
-                        <div class='card-body'>\n\
-                        <h5 class='card-title' style='display: inline-block;'>"+iterator[3]+"</h5>\n\
-                        <a href='https://www.politecnicojic.edu.co/images/downloads/facultades/ingenieria/programa-ingenieria-informatica-plan-10.jpg' target='_blank' rel='noopener noreferrer' style='display: inline-block; margin-left: 10px;'>(See the pensum of this course)</a>\n\
-                        <p class='card-text'>"+iterator[1]+"</p>\n\
-                        <h6>Actions:</h6>\n\
-                        <button class='btn btn-danger' data-bs-toggle='offcanvas' data-bs-target='#offcanvasExample' aria-controls='offcanvasExample' onclick=loadGroups('"+iterator[0]+"')>Groups</button>\n\
-                        <button class='btn btn-dark' style='width: 200px;'>Subjects</button>\n\
-                        </div>\n\
-                        </div>\n\
-                        </div>\n\
-                        </div>\n\
-                </div>";
         let modalElement="<div class='modal fade' id='ModalCourse"+iterator[0]+"' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>\n\
             <div class='modal-dialog'>\n\
         <div class='modal-content'>\n\
         <div class='modal-header'>\n\
             <h1 class='modal-title fs-5' id='exampleModalLabel'>Course material-"+iterator[3]+"</h1>\n\
-        <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close' onclick='cleanModalBtn()'></button>\n\
+        <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>\n\
         </div>\n\
         <div class='modal-body'>\n\
             <form>\n\
@@ -128,4 +110,14 @@ function cleanModalCourse(){
     document.getElementById("recipient-name").value=" ";
     document.getElementById("recipient-Image").value=" ";
     document.getElementById("message-Description").value=" ";
+}
+
+
+async function createSubjects(idCourse,name){
+    let SubjectGram={}
+    SubjectGram.name="Grammar-"+name;
+    SubjectGram.course=idCourse;
+    // SubjectGram.id=22;
+    console.log(SubjectGram);
+
 }
