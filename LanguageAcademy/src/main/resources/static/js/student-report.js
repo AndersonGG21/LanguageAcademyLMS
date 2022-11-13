a7975c53b44c93f573b82a1019c0d2a5a2129488
Luis david <david12.29@hotmail.com>
2022-11-12 23:17:53 -0500

    Add js and html about student-reports

---
+++ LanguageAcademy/src/main/resources/static/js/student-report.js
@@ -0,0 +1,13 @@
+window.addEventListener("load", () =>{
+    reportSale();
+})
+let colores=["#3371FF","#E6DF92","#BFF5D7","#E6CFF1",'#B833FF',"#CFD7F1","#FF33E0","#33FFF9","#9F33FF","#F1F99A","#BFF5D7"];
+let colores2=['#B833FF',"#FF33E0","#E6DF92","#BFF5D7","#E6CFF1","#3371FF","#CFD7F1","#33FFF9","#9F33FF","#F1F99A","#BFF5D7"];
+
+function reportSale() {
+    
+    
+
+    alert("gg");
+    
+}
