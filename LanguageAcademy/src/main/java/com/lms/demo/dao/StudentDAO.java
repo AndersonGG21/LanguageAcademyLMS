f275f1f269297cfe81b1043c1b5e0216e850af6c
Julian362 <Juliangarcia362@hotmail.com>
2022-11-16 13:25:00 -0500

    Add imports

--- LanguageAcademy/src/main/java/com/lms/demo/dao/StudentDAO.java
+++ LanguageAcademy/src/main/java/com/lms/demo/dao/StudentDAO.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
 package com.lms.demo.dao;
+import com.lms.demo.models.Course;
 import com.lms.demo.models.Student;
+import com.lms.demo.models.Group;
 import java.util.List;
 /**
  *
