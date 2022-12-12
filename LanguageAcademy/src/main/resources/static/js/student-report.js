f50cda3d1d27ed14a3c2c18f1123d9d4ea08c884
Luis david <david12.29@hotmail.com>
2022-12-12 01:34:14 -0500

    Update reports almost ready

--- LanguageAcademy/src/main/resources/static/js/student-report.js
+++ LanguageAcademy/src/main/resources/static/js/student-report.js
@@ -5,12 +5,7 @@ window.addEventListener("load", () =>{
 })
 
 
-// let colores=["#146e18","#d90429"];//otra opcion
-// let colores=["#A9B1AA","#F63F61"];// otra opcion 
-// let colores=["#43546F","#F63F61"];//  la mejor 
-// let colores=["#bcbce0","#F63F61"];
-let colores=["#d8c0ee","#F63F61"];
-// let colores=["#9764e9","#F63F61"];
+
 
 
 async function getData() {
@@ -28,34 +23,41 @@ async function getData() {
 
 }
 function reportSale(arrayCourse) {
+    // let colores=["#146e18","#d90429"];//otra opcion
+    //  let colores=["#A9B1AA","#F63F61"];// otra opcion 
+    // let colores=["#43546F","#F63F61"];//  la mejor 
+    // let colores=["#bcbce0","#F63F61"];
+    let colores=["#d8c0ee","#F63F61"];
+    // let colores=["#9764e9","#F63F61"];
+
+    // productValue=[10,2,8,4,13,2,3,13,2,4];
     
-    productValue=[10,2,8,4,13,2,3,13,2,4];
-    productValue2=[2,5,8,4,13,2,3,13,2,12];
-    productValue3=[8,2,8,4,9,2,3,12,2,4];
-    productValue4=[1,1,8,4,10,2,3,11,2,9];
-    productValue5=[4,9,8,4,9,2,3,13,2,4];
-    
-    product=["Grammar","Pending","Listening","Pending","Reading ","Pending","Speaking","Pending","Wrinting","Pending"];
-    labelsLanguages=["Grammar","Listening","Reading ","Speaking","Wrinting"];
-    Value=[10,22];
-    Progress=["Succes","Pending"];
+    // product=["Grammar","Pending","Listening","Pending","Reading ","Pending","Speaking","Pending","Wrinting","Pending"];
+    // labelsLanguages=["Grammar","Listening","Reading ","Speaking","Wrinting"];
+    labelsLanguages=["Grammar","Listening","Reading "];
+    // Value=[10,22];
+    Progress=["Succes","Failed or pending"];
     dataSucces=[1,2,3,4,5,6];
     dataPending=[9,5,4,3,2,1];
 
-    dataSucces2=[1,1,3,0,5,6];
-    dataPending2=[4,5,4,3,2,1];
-
-    dataSucces3=[1,3,3,0,4,6];
-    dataPending3=[1,5,4,3,8,1];
-
-    dataSucces4=[3,2,4,5,2,3];
-    dataPending4=[1,0,2,2,1,1];
-    
+    let k=0;
+    arrayCourse.map((element)=>{
+        let arrayAux=[];
+        let arrayAuxWrog=[];
+        arrayAux.push(element[4]);
+        arrayAux.push(element[5]);
+        arrayAux.push(element[6]);
+        arrayAuxWrog.push(5-element[4]);
+        arrayAuxWrog.push(5-element[5]);
+        arrayAuxWrog.push(5-element[6]);
+        
+        generatestackedBars(colores,labelsLanguages,arrayAux,arrayAuxWrog,"MiGrafica"+k);
+        k++;
+        
+    })
+   
     // generateBarPie(Progress,Value,colores,'MiGrafica');
-    generatestackedBars(colores,labelsLanguages,dataSucces4,dataPending4,"MiGrafica2");
-    // generatestackedBars(colores,labelsLanguages,dataSucces3,dataPending3,"MiGrafica3");
-    // generatestackedBars(colores,labelsLanguages,dataSucces2,dataPending2,"MiGrafica4");
-    // generatestackedBars(colores,labelsLanguages,dataSucces,dataPending,"MiGrafica5");
+    // generatestackedBars(colores,labelsLanguages,dataSucces4,dataPending4,"MiGrafica2");
 }
 
 function generateBarChart(product,text,colores,value,id){
@@ -105,7 +107,7 @@ function generatestackedBars(color,labelsLanguages,dataSucces,dataPending,id) {
           borderColor: color[0],
           borderWidth: 1
         },{
-          label:'Pending',
+          label:'Failed or pending',
           data: dataPending,
           backgroundColor: color[1],
           borderColor: color[1],
@@ -137,9 +139,8 @@ function generatestackedBars(color,labelsLanguages,dataSucces,dataPending,id) {
     let arrayCourse=[];
     let arrayCourseColor=[];
     filterArrayCourses(noteFilter,arrayCourse,arrayCourseColor);
-    // console.log(arrayCourse);
+    
     let numCourse=arrayCourse.length;
-    // console.log(arrayCourseColor);
     let containerCanvas="<div>\n\
                 <h1>"+name +" reports</h1>\n\
                 <p>Here you can see reports of your grades</p>\n\
@@ -187,23 +188,3 @@ function filterArrayCourses(noteFilter,arrayCourse,arrayCourseColor) {
 }
 
 
-{/* <div class='divReport2'>\n\
-                        <h3>Total progress in the English language</h3>\n\
-                        <canvas id='MiGrafica' width='100' height='100' class='canvasPie'> </canvas>\n\
-                    </div>\n\
-                    <div class='divReport'>\n\
-                        <h3>A1 progress</h3>\n\
-                        <canvas id='MiGrafica2' width='100' height='100'> </canvas>\n\
-                    </div>\n\
-                    <div class='divReport'>\n\
-                        <h3>A2 progress</h3>\n\
-                        <canvas id='MiGrafica3' width='100' height='100'> </canvas>\n\
-                    </div>\n\
-                    <div class='divReport'>\n\
-                        <h3>B1 progress</h3>\n\
-                        <canvas id='MiGrafica4' width='100' height='100'> </canvas>\n\
-                    </div>\n\
-                    <div class='divReport'>\n\
-                        <h3>B2 progress</h3>\n\
-                        <canvas id='MiGrafica5' width='100' height='100'> </canvas>\n\
-                    </div> */}
